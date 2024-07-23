60120ca6bdb52432da14df6f05e1498cdd223167
gopinathgbg <gopinath.vbg@gmail.com>
2024-07-24 00:08:06 +0530

    Account Management microservice created

---
+++ src/main/java/com/hmis/accountservice/Account/AccountController.java
@@ -0,0 +1,15 @@
+package com.hmis.accountservice.Account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+	@GetMapping("state/check")
+	public String account() {
+		return "Working ";
+	}
+}
